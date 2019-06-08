0793ad03e526441149edd56937ce1f7eb72d9019
Dustin Cintron <D.Cint88@gmail.com>
2019-06-08 11:44:16 -0400

    Deploy App to Heroku, github instructions7

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(proecss.env.PORT || 8080);
 
