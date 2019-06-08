97675187c481fbcc583b2175b8e3584b6702e197
Dustin Cintron <D.Cint88@gmail.com>
2019-06-07 22:50:44 -0400

    Deploy App to Heroku, Turbo instructions4

---
+++ dist/out-tsc/src/app/homepage/homepage.component.js
@@ -0,0 +1,15 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+var HomepageComponent = /** @class */ (function () {
+    function HomepageComponent() {
+    }
+    HomepageComponent = tslib_1.__decorate([
+        Component({
+            selector: 'home',
+            template: '<h1>Freelance Bootcamp Homepage</h1>'
+        })
+    ], HomepageComponent);
+    return HomepageComponent;
+}());
+export { HomepageComponent };
+//# sourceMappingURL=homepage.component.js.map
