caefd0b27511decd4763a12e495928983d7b67a8
Dustin Cintron <D.Cint88@gmail.com>
2019-06-08 14:46:10 -0400

    Deploy App to Heroku, medium.com instructs

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+// Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files from the dist directory
+app.use(express.static(__dirname + '/dist/freelance-camp-fe'));
+
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/dist/freelance-camp-fe/index.html'));
+})
+
+// Start the app by listening on the default heroku port
 app.listen(process.env.PORT || 8080);
