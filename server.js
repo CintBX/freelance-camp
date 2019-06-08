1a29bfcb3787230d815c8aa2091d6f1146b33c9b
Dustin Cintron <D.Cint88@gmail.com>
2019-06-08 14:59:40 -0400

    Fixing layout3

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/freelance-camp-fe'));
+app.use(express.static(__dirname + '/src/freelance-camp-fe'));
 
 app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/src/index.html'));
+	res.sendFile(path.join(__dirname + '/src/freelance-camp-fe/index.html'));
 })
 
 // Start the app by listening on the default heroku port
