806e3535be7c068ffa91bc08315cf4fb36d8cec2
Dustin Cintron <D.Cint88@gmail.com>
2019-06-08 14:06:57 -0400

    Deploy App to Heroku, ng serve instead of server.js

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
-
-app.listen(proecss.env.PORT || 8080);
-
-//PATH location strategy
-
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/src/index.html'));
-})
+app.listen(process.env.PORT || 8080);
