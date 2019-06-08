0fec74e3e0a99e63cc52bfb3723b1330431e4aad
Dustin Cintron <D.Cint88@gmail.com>
2019-06-08 15:02:01 -0400

    Fixing layout4

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/src/freelance-camp-fe'));
+app.use(express.static(__dirname + '/dist/freelance-camp-fe'));
 
 app.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/src/freelance-camp-fe/index.html'));
