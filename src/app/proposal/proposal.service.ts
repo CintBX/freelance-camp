b3d2d74de8bb3500456bc72e381d08414af7f029
Dustin Cintron <D.Cint88@gmail.com>
2019-06-09 22:58:06 -0400

    Debugging heroku more2

--- src/app/proposal/proposal.service.ts
+++ src/app/proposal/proposal.service.ts
@@ -6,7 +6,7 @@ import { Proposal } from './proposal';
 
 @Injectable()
 export class ProposalService {
-	private proposalsUrl = 'http://localhost:3002/proposals';
+	private proposalsUrl = 'https://cintron-freelance-proposals.herokuapp.com/';
 
 	constructor(
 		private http: Http
