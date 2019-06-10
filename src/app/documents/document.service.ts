34d9597119c1d2860ee5595f5c912b4a0a725c92
Dustin Cintron <D.Cint88@gmail.com>
2019-06-09 22:49:50 -0400

    Debugging heroku more1

--- src/app/documents/document.service.ts
+++ src/app/documents/document.service.ts
@@ -6,7 +6,7 @@ import { Document } from './document';
 
 @Injectable()
 export class DocumentService {
-	private documentsUrl = 'http://localhost:3001/freelance_documents';
+	private documentsUrl = 'https://cintron-freelance-documents.herokuapp.com/';
 
 	constructor(
 		private http: Http
