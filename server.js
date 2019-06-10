c7bf3ce32e06af34e366479d535ee04ed9379af4
Dustin Cintron <D.Cint88@gmail.com>
2019-06-09 22:38:25 -0400

    Debugging heroku 10:

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const path = require('path');
 
 const app = express();
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist/freelance-camp-fe'));
 
