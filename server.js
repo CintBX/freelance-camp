6da70de20d1a05ff43e2e12d0f403b12aa735034
Dustin Cintron <D.Cint88@gmail.com>
2019-06-11 11:45:42 -0400

    CHanged it back to 8080, deleted forceSsl code

--- server.js
+++ server.js
@@ -4,19 +4,6 @@ const path = require('path');
 
 const app = express();
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-
-app.use(forceSSL());
-
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist/freelance-camp-fe'));
 
@@ -25,4 +12,4 @@ app.get('/*', function(req, res) {
 })
 
 // Start the app by listening on the default heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
