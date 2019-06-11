8dbf3cccc93e5ced361d6f12aa49969255ac8f03
Dustin Cintron <D.Cint88@gmail.com>
2019-06-11 11:55:23 -0400

    replaced dirname code

--- server.js
+++ server.js
@@ -17,5 +17,12 @@ const forceSSL = function() {
 
 app.use(forceSSL());
 
+// Serve only the static files from the dist directory
+app.use(express.static(__dirname + '/dist/freelance-camp-fe'));
+
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/freelance-camp-fe/src/index.html'));
+})
+
 // Start the app by listening on the default heroku port
 app.listen(process.env.PORT || 8080);
