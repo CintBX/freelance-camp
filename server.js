c278f3d9da4ddf903e746fad05385feae5158f6c
Dustin Cintron <D.Cint88@gmail.com>
2019-06-11 11:49:24 -0400

    Replaced forceSSL code; deleted dirname code

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/freelance-camp-fe'));
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
 
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/freelance-camp-fe/src/index.html'));
-})
+app.use(forceSSL());
 
 // Start the app by listening on the default heroku port
 app.listen(process.env.PORT || 8080);
