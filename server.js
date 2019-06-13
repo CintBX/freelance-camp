40ebe89c4dd5f14236a2c5cb2fd301183d3edace
Dustin Cintron <D.Cint88@gmail.com>
2019-06-13 08:36:42 -0400

    Re-entered index.html; removed appname from dirs

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ const forceSSL = function() {
 app.use(forceSSL());
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/freelance-camp-fe/dist'));
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/src/index.html'));
+})
 
 // Start the app by listening on the default heroku port
 app.listen(process.env.PORT || 8080);
