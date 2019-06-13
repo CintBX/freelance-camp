d21f99291bab594dd647a6ffe1ef7d25f6e7ab78
Dustin Cintron <D.Cint88@gmail.com>
2019-06-13 08:45:04 -0400

    src/app/index AGAIN

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(forceSSL());
 app.use(express.static(__dirname + '/freelance-camp-fe/dist'));
 
 app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/freelance-camp-fe/src/index.html'));
+	res.sendFile(path.join(__dirname + '/src/freelance-camp-fe/index.html'));
 })
 
 // Start the app by listening on the default heroku port
