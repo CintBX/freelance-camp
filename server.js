2a4244ce26ca1b5cf4e718a1a0da9a74907decc8
Dustin Cintron <D.Cint88@gmail.com>
2019-06-13 08:28:04 -0400

    server.js appname/dist instead of dist/appname

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const forceSSL = function() {
 app.use(forceSSL());
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/freelance-camp-fe'));
+app.use(express.static(__dirname + '/freelance-camp-fe/dist'));
 
 app.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/freelance-camp-fe/src/index.html'));
