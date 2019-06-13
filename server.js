9f868bab54ea7bc82afed243822b7f6cbf7b2dd3
Dustin Cintron <D.Cint88@gmail.com>
2019-06-13 08:50:46 -0400

    reverted server.js back to yesterday's state

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ const forceSSL = function() {
 app.use(forceSSL());
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/freelance-camp-fe/dist'));
+app.use(express.static(__dirname + '/dist/freelance-camp-fe'));
 
 app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/src/freelance-camp-fe/index.html'));
+	res.sendFile(path.join(__dirname + '/freelance-camp-fe/src/index.html'));
 })
 
 // Start the app by listening on the default heroku port
