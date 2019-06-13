e7769f86c262050ab78c98db60b2665caa626ebc
Dustin Cintron <D.Cint88@gmail.com>
2019-06-13 08:41:22 -0400

    Put appname back in dirs

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ const forceSSL = function() {
 app.use(forceSSL());
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/freelance-camp-fe/dist'));
 
 app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/src/index.html'));
+	res.sendFile(path.join(__dirname + '/freelance-camp-fe/src/index.html'));
 })
 
 // Start the app by listening on the default heroku port
